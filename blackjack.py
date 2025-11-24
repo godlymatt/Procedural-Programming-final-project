794204d46f9d1a64189fa72bace83fed95bb7d91
godlymatt <doofymatt@gmail.com>
2025-11-24 13:45:01 -0330

    Add files via upload

---
+++ blackjack.py
@@ -0,0 +1,20 @@
+def title():
+    print("Welcome to matt's super amazing Blackjack game!!!")
+    print("-------------------------------------------------")
+    print("If you win the payout is a whopping X1.5 your bet!!!")
+
+
+
+
+
+
+
+
+
+
+def main():
+    title()
+
+
+if __name__ == "__main__":
+    main()
