31709c366b2d9f4487e2bc27006c46ab3b4bc60c
Matthew Bruce <Matthew.Bruce@k202domain.csn.com>
2025-11-27 11:25:19 -0330

    Added deck function, imported random and started player hand

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,5 @@
+import random
+
 def title():
     print("Welcome to matt's super amazing Blackjack game!!!")
     print("-------------------------------------------------")
@@ -6,16 +8,33 @@ def title():
 
 
 
+def get_deck():
+    deck = []
+    ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    suits = ["\u2660", "\u2665\uFE0F", "\u2666\uFE0F", "\u2663"]
+    for rank in ranks:
+        for suit in suits:
+            deck.append((suit, rank))
+    print(deck)
+    return deck
 
 
-
+def player_hand(deck):
+        player = []
+        rand_int = random.randint(1, len(deck))
+        chosen_card = deck[rand_int]
+        player.append(chosen_card)
+        deck.pop(rand_int)
+        print(player)
+        print(deck)
 
 
 
 
 def main():
     title()
-
+    deck = get_deck()
+    player_hand(deck)
 
 if __name__ == "__main__":
     main()
