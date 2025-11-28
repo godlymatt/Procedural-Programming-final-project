baac4677c73ed1962e5a4eba5be3c2e19a8ca6d7
Matthew Bruce <Matthew.Bruce@k202domain.csn.com>
2025-11-28 11:59:53 -0330

    Fixed deck to add values to cards and started point system

--- blackjack.py
+++ blackjack.py
@@ -11,30 +11,42 @@ def title():
 def get_deck():
     deck = []
     ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     suits = ["\u2660", "\u2665\uFE0F", "\u2666\uFE0F", "\u2663"]
-    for rank in ranks:
-        for suit in suits:
-            deck.append((suit, rank))
+    for suit in suits:
+        for value, rank in enumerate(ranks):
+            deck.append([suit, rank, values[value]])
     print(deck)
     return deck
 
 
 def player_hand(deck):
-        player = []
-        rand_int = random.randint(1, len(deck))
-        chosen_card = deck[rand_int]
-        player.append(chosen_card)
-        deck.pop(rand_int)
-        print(player)
-        print(deck)
+    player = []
+    rand_int = random.randint(1, len(deck))
+    chosen_card = deck[rand_int]
+    player.append(chosen_card)
+    deck.pop(rand_int)
+    print(player)
+    print(deck)
 
+def player_points(chosen_card, player):
+    total_points = 0
+    for 
 
+def dealer_hand(deck):
+    dealer = []
+    rand_int = random.randint(1, len(deck))
+    chosen_card = deck[rand_int]
+    dealer.append(chosen_card)
+    deck.pop(rand_int)
+    print(dealer)
+    print(deck)
 
 
 def main():
     title()
     deck = get_deck()
     player_hand(deck)
-
+    dealer_hand(deck)
 if __name__ == "__main__":
     main()
