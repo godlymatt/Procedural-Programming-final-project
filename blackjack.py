19ef47690ef45017c19a06f9bf0405494c13a35c
Matthew Bruce <Matthew.Bruce@k202domain.csn.com>
2025-12-04 08:56:56 -0330

    Added dealer point system, based off first player points and fixed player points so user has an option.

--- blackjack.py
+++ blackjack.py
@@ -11,7 +11,7 @@ def title():
 def get_deck():
     deck = []
     ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
-    values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    values = [(1,11), 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     suits = ["\u2660", "\u2665\uFE0F", "\u2666\uFE0F", "\u2663"]
     for suit in suits:
         for value, rank in enumerate(ranks):
@@ -22,7 +22,7 @@ def get_deck():
 
 def player_hand(deck):
     player = []
-    rand_int = random.randint(1, len(deck))
+    rand_int = random.randint(1, len(deck)-1)
     chosen_card = deck[rand_int]
     player.append(chosen_card)
     deck.pop(rand_int)
@@ -30,25 +30,46 @@ def player_hand(deck):
     print(deck)
     return player
 
-def player_points(player):
-    total_points = 0
+
+def dealer_points(dealer):
+    dealer_total_points = 0
     aces = 0
-    for card in player:
-        card_value = card[2]
+    for card in dealer:
+        card_value =card[2]
         if card[1] == "Ace":
             aces += 1
-            total_points += 1
+            dealer_total_points += 1
         else:
-            total_points += card_value
-    while aces > 0 and total_points + 10 <= 21:
-        total_points += 10
-        aces -= 1
-    return total_points
+            dealer_total_points += card_value
+        while aces > 0 and dealer_total_points + 10 <= 21:
+            dealer_total_points += 10
+            aces -= 1
+        return dealer_total_points
+
+
+
+def player_points(player):
+    player_total_points = 0
+    for card in player:
+        if card[1] == "Ace":
+            if player_total_points <= 10:
+                while True:
+                    ace_choice = input("You have drawn an ace. Would you like it to be 1 or 11 points?: ")
+                    if ace_choice == "1" or ace_choice == "11":
+                        player_total_points += int(ace_choice)
+                        break
+                    else:
+                        print("You must pick between 1 and 11. Try again.")
+            else:
+                player_total_points += 1
+        else:
+            player_total_points += card[2]
+    return player_total_points
 
 
 def dealer_hand(deck):
     dealer = []
-    rand_int = random.randint(1, len(deck))
+    rand_int = random.randint(1, len(deck)-1)
     chosen_card = deck[rand_int]
     dealer.append(chosen_card)
     deck.pop(rand_int)
